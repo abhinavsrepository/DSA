984e7d7247abfa782404f8d0b1c5efc896ce5ed8
Abhinav Singh <abhi2510979b@gmail.com>
2025-06-21 19:16:33 +0530

    initiak

---
+++ java/Minipeaks.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Minipeaks{
+public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter the number of elements");
+    int n =sc.nextInt();   
+    int a[] = new int[n];
+    System.out.println("enter the eke"); 
+}
+}
