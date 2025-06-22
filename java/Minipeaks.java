659da044545230f0c732c138a50a6e4ae4d9b7f7
Abhinav Singh <abhi2510979b@gmail.com>
2025-06-22 23:54:29 +0530

    initial commit

--- java/Minipeaks.java
+++ java/Minipeaks.java
@@ -8,5 +8,16 @@ public static void main(String[] args) {
     int n =sc.nextInt();   
     int a[] = new int[n];
     System.out.println("enter the eke"); 
+
+    for (int i=0;i<a.length-1;i++){
+        a[i]= sc.nextInt();
+    }
+    for (int i=0;i<a.length -1 ;i++){
+        if(a[i]>a[i-1]&& a[i]>a[i+1]){
+            System.out.println(a[i]);
+        }
+    }
+
+
 }
 }
