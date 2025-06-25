1940d1c93443258e20a817710d58fc6906346b8f
Abhinav Singh <abhi2510979b@gmail.com>
2025-06-26 00:13:12 +0530

    linked list completed

---
+++ java/LinkedListColl.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class LinkedListColl {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+
+        list.addFirst("a");
+        list.addFirst("is");
+        System.out.println(list);
+        list.addFirst("this");
+        list.addLast("list");
+    System.out .println(list);
+    System.out.println(list.size());
+    for(int i=0;i<list.size();i++){
+        System.out.println(list.get(i)+"->");
+        System.out.println("null");
+
+    }
+
+    }
+    
+}
